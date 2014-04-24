3f2a2bde0611191b1f54627818299305e0b70491
Chris Allen <cehallen@gmail.com>
2014-04-24 11:47:25 -0400

    Readded add subtitle button

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Mce::Application.routes.draw do
 
   root 'movies#index'
   
-  resources :movies, except: [:new] do
+  resources :movies do
     resources :words, only: [:show]
   end  
   resources :sentences,  only: [:show]
